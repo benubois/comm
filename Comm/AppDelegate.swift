307efadd8b39dce32bcd0eb0c67b702b80431853
Ben Ubois <ben@benubois.com>
2015-09-16 20:44:11 -0700

    Initial Commit

---
+++ Comm/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Comm
+//
+//  Created by ben on 9/16/15.
+//  Copyright (c) 2015 Ben Ubois. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Comm/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Comm
+//
+//  Created by ben on 9/16/15.
+//  Copyright (c) 2015 Ben Ubois. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
