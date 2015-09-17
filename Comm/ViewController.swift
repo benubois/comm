e49767a8693d0136d49ab0114b8781243c2d1c12
Ben Ubois <ben@benubois.com>
2015-09-16 22:57:46 -0700

    Initial commit.

--- Comm/ViewController.swift
+++ Comm/ViewController.swift
@@ -6,22 +6,44 @@
 //  Copyright (c) 2015 Ben Ubois. All rights reserved.
 //
 
+import Foundation
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSTextViewDelegate {
 
+    @IBOutlet var listOne: NSTextView!
+    @IBOutlet var listTwo: NSTextView!
+    @IBOutlet var output: NSTextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        listOne.delegate = self
+        listTwo.delegate = self
     }
 
-    override var representedObject: AnyObject? {
-        didSet {
-        // Update the view, if already loaded.
+    func textDidChange(notification: NSNotification) {
+        if let object = notification.object as? NSTextView {
+            if object == listOne || object == listTwo {
+                if let listOneText = listOne.string, listTwoText = listTwo.string {
+                    comm(listOneText, stringTwo: listTwoText)
+                }
+            }
         }
     }
-
+    
+    func comm(stringOne: String, stringTwo: String) {
+        let setOne = stringToSet(stringOne)
+        let setTwo = stringToSet(stringTwo)
+        let common = setOne.intersect(setTwo)
+        let commonString = "\n".join(common)
+        output.string = commonString
+    }
+    
+    func stringToSet(string: String) -> Set<String> {
+        let array = string.componentsSeparatedByString("\n")
+        let set = Set(array)
+        return set
+    }
 
 }
 
